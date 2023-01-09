23ec47afc724a9cd896f038f7f7e7ea5ecdb5a8a
bilal <bilalqwider@yahoo.com>
2023-01-09 10:41:12 +0200

    add home page and footer

--- project/urls.py
+++ project/urls.py
@@ -21,10 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('property/',include('property.urls',namespace='property')),
-    path('blog/',include('blog.urls',namespace='blog')),
+    path('blog/',include('blog.urls' ,namespace='blog')),
+    path('',include('settings.urls' ,namespace='home')), 
     path('summernote/', include('django_summernote.urls')),
-
-
 ] 
 
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
